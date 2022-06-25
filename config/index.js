17255d256429067d065728c5daa20cabfbb240c1
John Adamsy <adams.opiyo@gmail.com>
2022-06-25 08:09:11 +0300

    chore: initialized test coverage and app configs

---
+++ config/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Load Module dependencies.
+ */
+
+const env = process.env;
+
+const PORT      = env.PORT || 6500;
+const API_URL   = env.API_URL || 'http://127.0.0.1:6500';
+const NODE_ENV  = env.NODE_ENV || 'development';
+const HOST      = env.HOST_IP || 'localhost';
+
+let config = {
+
+    // Root Configs
+    API_URL: API_URL,
+
+    ENV: NODE_ENV,
+
+    PROCESS_ENV: env,
+
+    PORT: PORT,
+
+    HOST: HOST,
+
+    TIME_ZONE: 'Africa/Nairobi',
+    OK_STATUS: [200,201,202,204,208]
+};
+
+module.exports = config;
