b6f58b602eb06aea6b8ad67837ca99fc7860db6d
John Adamsy <adams.opiyo@gmail.com>
2022-07-01 06:49:42 +0300

    test(routes): test wrong endpoint, and the three main endpoints

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -19,3 +19,38 @@ describe('Test Default Endpoint', () => {
  expect(response.body.name).toContain('hacker-news-app');
  });
 });
+
+describe('Test Wrong Endpoint', () => {
+    test('get wrong route GET /words', async () => {
+    const response = await request(server).get('/words');
+    expect(response.status).toEqual(404);
+    expect(response.text).toContain('Not Found');
+    });
+   });
+
+describe('Test words/occurrences Endpoint', () => {
+    //jest.setTimeout(2500);
+    test('get 10 most occurring words from last 25 stories GET /words/occurrences', async () => {
+    const response = await request(server).get('/words/occurrences');
+    expect(response.status).toEqual(200);
+    expect(Object.keys(response.body)).toEqual(['data','title']);
+ });
+});
+
+describe('Test words/occurrences/last-week Endpoint', () => {
+    jest.setTimeout(10000);
+    test('get 10 most occurring words from posts of last week GET /words/occurrences/last-week', async () => {
+    const response = await request(server).get('/words/occurrences/last-week');
+    expect(response.status).toEqual(200);
+    expect(Object.keys(response.body)).toEqual(['data','title','posts']);
+ });
+});
+
+describe('Test words/occurrences/users Endpoint', () => {
+    jest.setTimeout(30000);
+    test('get 10 most occurring words from last stories of users with karma > 10000 GET /words/occurrences/users', async () => {
+    const response = await request(server).get('/words/occurrences/users');
+    expect(response.status).toEqual(200);
+    expect(Object.keys(response.body)).toEqual(['data','title','users','posts']);
+ });
+});
